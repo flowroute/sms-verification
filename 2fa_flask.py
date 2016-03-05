50f4f3eb6b7ae62f66e3cbde5bc0a2c54519b3fc
Casey MacPhee <cmacphee@flowroute.com>
2016-03-04 16:28:25 -0800

    Refactor Flask app view. Make flake8 happy. Add requirements.txt.

--- 2fa_flask.py
+++ 2fa_flask.py
@@ -1,46 +1,57 @@
-from flask import Flask, render_template, request, flash
-from flask.ext.wtf import Form
-from wtforms import TextField, SubmitField, validators, ValidationError
-from FlowrouteMessagingLib.Controllers.APIController import *
-from FlowrouteMessagingLib.Models import * 
 from random import randint
+from flask import Flask, render_template, request
+from flask.ext.wtf import Form
+from wtforms import TextField, SubmitField, validators
 
-controller = APIController(username="AccessKey", password="SecretKey")       
+from FlowrouteMessagingLib.Controllers.APIController import APIController
+from FlowrouteMessagingLib.Models import Message
+
+
+global SECRET_CODE
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'Your_app_secret_key'
 
-@app.route("/", methods=['GET', 'POST'])
-def main():
+controller = APIController(username="AccessKey", password="SecretKey")
 
-  global secretCode
 
-  class UserForm(Form):
-    toNumber = TextField("Phone Number", [validators.required("Please enter your cell phone number"), validators.Length(min=11, max=11)])
+class UserForm(Form):
+    """
+    Validates user input to ensure the phone number is of proper length.
+    Exposes a TextField where the user can submit the secret code.
+    """
+    toNumber = TextField(
+        "Phone Number",
+        [validators.required("Please enter your cell phone number"),
+            validators.Length(min=11, max=11)])
     submit = SubmitField("Send Confirmation Code")
     pinField = TextField("Secret Code")
     checkPin = SubmitField("Validate your pin")
-  
-  form = UserForm() 
 
-  if request.method == 'GET':
-    return render_template('index.html', form=form)
 
-  elif request.method == 'POST':
-    if form.validate() == False:
-          return render_template('index.html', form=form)
-    elif 'submit' in request.form:  
-      secretCode = str(randint(999,9999))
-      recipient = str(form.toNumber.data)
-      msg = Message(to=recipient, from_="18444205780", content=secretCode)
-      response = controller.create_message(msg)
-      return render_template('index.html', form=form)         
-    else:
-      if str(form.pinField.data) == secretCode:
-        return "Pin validated successfully"
-      else:
-        return "Pin validation failed"
+@app.route("/", methods=['GET', 'POST'])
+def user_verification():
+    """
+    Renders the 2 factor authentication form, generates a secret code, 
+    and verifies they are equal on submission.
+    """
+    form = UserForm()
+    if request.method == 'GET':
+        return render_template('index.html', form=form)
+    elif request.method == 'POST':
+        if form.validate() is False:
+            return render_template('index.html', form=form)
+        elif 'submit' in request.form:
+            SECRET_CODE = str(randint(999, 9999))
+            recipient = str(form.toNumber.data)
+            msg = Message(to=recipient, from_="18444205780", content=SECRET_CODE)
+            controller.create_message(msg)
+            return render_template('index.html', form=form)
+        elif str(form.pinField.data) == SECRET_CODE:
+            return "Pin validated successfully"
+        else:
+            return "Pin validation failed"
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# two_factor_auth_python
+To run this application you must first have the FlowrouteMessagingLib at the 
+top level of this directory, and it's dependencies installed.
+
+Run 'pip install -r requirements.txt' to install dependencies
+Run 'python -m 2fa_flask' to start the flask app
