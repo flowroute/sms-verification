0a67ee60193c7768bd636550610b7f8ada9585b7
Casey MacPhee <cmacphee@flowroute.com>
2016-04-29 14:10:02 -0700

    Add a simple requests client.

---
+++ sms_auth_service/client.py
@@ -0,0 +1,40 @@
+import json
+
+import requests
+
+
+SMS_AUTH_ENDPOINT = 'http://localhost:5000'
+
+
+class SMSAuthClient(object):
+
+    def __init__(self, endpoint=SMS_AUTH_ENDPOINT):
+        self.endpoint = endpoint
+
+    def create_auth(self, auth_id, recipient):
+        payload = {'auth_id': auth_id,
+                   'recipient': recipient}
+        response = requests.post(self.endpoint, json=payload)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError, e:
+            import pdb; pdb.set_trace()
+            content = e.response.json()
+            e.message = content['message']
+            e.strerror = content['reason']
+            raise e
+
+        return response.json()
+
+    def authenticate_code(self, auth_id, code):
+        args = {'auth_id': auth_id,
+                'code': code}
+        response = requests.get(self.endpoint, params=args)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError, e:
+            content = e.response.json()
+            e.message = content['message']
+            e.strerror = content['reason']
+            raise e
+        return response.json()
